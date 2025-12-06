de4a9b824beb82954f8767af7a99e211289bbd97
AmirMahdiyar <amirmahdiyarshirijiyan@gmail.com>
2025-12-06 20:44:04 +0330

    snapshots

--- Mokeb.Host/Mokeb.Infrastructure/Migrations/20251206171309_addingBloodTypeColumn.Designer.cs
+++ Mokeb.Host/Mokeb.Infrastructure/Migrations/20251206171309_addingBloodTypeColumn.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Mokeb.Infrastructure.Context;
 
@@ -11,9 +12,11 @@ using Mokeb.Infrastructure.Context;
 namespace Mokeb.Infrastructure.Migrations
 {
     [DbContext(typeof(MokebDbContext))]
-    partial class MokebDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251206171309_addingBloodTypeColumn")]
+    partial class addingBloodTypeColumn
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
