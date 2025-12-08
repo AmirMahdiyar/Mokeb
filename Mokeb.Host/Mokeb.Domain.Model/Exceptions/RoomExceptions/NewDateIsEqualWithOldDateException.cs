2bedf3213bb1c36846d48be6b2a3526d13e151b4
AmirMahdiyar <amirmahdiyarshirijiyan@gmail.com>
2025-12-08 13:13:40 +0330

    تغییرات:

--- Mokeb.Host/Mokeb.Domain.Model/Exceptions/RoomExceptions/NewDateIsEqualWithOldDateException.cs
+++ Mokeb.Host/Mokeb.Domain.Model/Exceptions/RoomExceptions/NewDateIsEqualWithOldDateException.cs
@@ -2,7 +2,7 @@
 
 namespace Mokeb.Domain.Model.Exceptions.RoomExceptions
 {
-    public class NewDateIsEqualWithOldDateException : NotChangesDomainException
+    public class NewDateIsEqualWithOldDateException : NoChangesDomainException
     {
         public NewDateIsEqualWithOldDateException() : base("New Date can't be equal with old date") { }
     }
